132f2a50f97a7d346cff6a3a05de1a9db5c74fec
Marcos Oliveira <marcosoliveeira@hotmail.com>
2023-11-15 02:09:10 -0300

    fix: typo

--- frontend/src/components/svg/checked-svg.tsx
+++ frontend/src/components/svg/checked-svg.tsx
@@ -1,2 +1,2 @@
 export const CheckedSvg = () => (
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-slate-500"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path></svg>)
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-slate-500"><path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5"></path></svg>)
--- frontend/src/components/svg/unchecked-svg.tsx
+++ frontend/src/components/svg/unchecked-svg.tsx
@@ -1,3 +1,3 @@
 export const UncheckedSvg = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-slate-500 hover:text-sky-600 hover:cursor-pointer"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-slate-500 hover:text-sky-600 hover:cursor-pointer"><path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
 )
